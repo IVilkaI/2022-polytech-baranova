fe94bccbcafce2e5a0cf95c46a440ed26f0e8244
IVilkaI <112807763+IVilkaI@users.noreply.github.com>
2022-10-29 01:30:29 +0300

    feat:04array_inserion done

--- 04_search_sort/array_insertion/main.cpp
+++ 04_search_sort/array_insertion/main.cpp
@@ -12,6 +12,19 @@ void insert(int x, int p, int* arr, int &n) {
     n += 1;
 }
 
+int search_position(int x, const int* arr, int n) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] >= x) {
+            return i;
+        }
+    }
+    return n;
+}
+
+void insert_sorted(int x, int* arr, int &n) {
+    insert(x, search_position(x, arr, n), arr, n);
+}
+
 void print(int* arr, int n) {
     for (int i = 0; i < n; i++) {
         std::cout << arr[i] << " ";
@@ -23,9 +36,35 @@ int main() {
     for (int i = 0; i < N; i++) {
         A[i] = i;
     }
-
+    std::cout << "insert function" << std::endl;
+    std::cout << "Initial array: " << std::endl;
     print(A, N);
-    insert(99, 3, A, N);
+    insert(99, 4, A, N);
+    std::cout << "Array after insertion: " << std::endl;
+    print(A, N);
+    std::cout << std::endl;
+
+    std::cout << "search_position function" << std::endl;
+    std::cout << "Initial array: " << std::endl;
+    for (int i = 0; i < N; i++) {
+        A[i] = i * 2;
+    }
+    print(A, N);
+    std::cout << "99 must be on position " << search_position(99, A, N) << std::endl;
+    std::cout << "3 must be on position " << search_position(3, A, N) << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "insert_sorted function" << std::endl;
+    std::cout << "Initial array: " << std::endl;
+    for (int i = 0; i < N; i++) {
+        A[i] = i * 2;
+    }
+    print(A, N);
+    insert_sorted(99, A, N);
+    std::cout << "Array after insertion: " << std::endl;
+    print(A, N);
+    insert_sorted(3, A, N);
+    std::cout << "Array after insertion: " << std::endl;
     print(A, N);
     return 0;
 }
