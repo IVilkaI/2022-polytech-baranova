227c9a388999157f30652d9b2cfec6a19611c9d6
IVilkaI <112807763+IVilkaI@users.noreply.github.com>
2022-11-30 22:25:10 +0300

    feat: train done

---
+++ 06_pointers_references/intro/main.cpp
@@ -0,0 +1,18 @@
+#include <cassert>
+#include <cstring>
+
+unsigned int length(const char* str)
+
+{
+    return  strlen(str);
+}
+
+int main()
+{
+    assert(length("a") == 1);
+    assert(length("ab") == 2);
+    assert(length("") == 0);
+    assert(length("hello world") == 11);
+    assert(length(nullptr) == 0);
+    return 0;
+}
