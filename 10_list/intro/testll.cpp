8dbc5895e6fdc18d8cdef03a5657552476cf44e9
IVilkaI <112807763+IVilkaI@users.noreply.github.com>
2022-12-04 21:12:43 +0300

    fix:test fixed

--- 10_list/intro/testll.cpp
+++ 10_list/intro/testll.cpp
@@ -32,7 +32,14 @@ TEST_CASE("MyList") {
     CHECK_EQ(list.get(1)->data, list2.get(1)->data);
     CHECK_EQ(list.get(2)->data, list2.get(2)->data);
 
+    MyList list3 = {1, 2, 3};
+    CHECK_EQ(list3.size(), 3);
+    CHECK_EQ(list3.get(0)->data, 1);
+    CHECK_EQ(list3.get(1)->data, 2);
+    CHECK_EQ(list3.get(2)->data, 3);
+
     std::cout << "list: " << std::endl << list << std::endl;
     std::cout << "list2: " << std::endl  << list2 << std::endl;
+    std::cout << "list3: " << std::endl  << list3 << std::endl;
 }
 
